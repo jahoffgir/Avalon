c6bfad5d294206b1860c090907217fb2e547cc0e
Jahongir A <jxa7578@g.rit.edu>
2017-10-08 07:26:23 -0400

    Refactered code for Board

--- js/board.js
+++ js/board.js
@@ -1,30 +1,12 @@
-// list of quests that players went to.
-var quests = [];
-
-// Assigning people to go on quests and whether they were success/fail.
-// @param lst - list of people attending the game
-// @param bool - success/fail of the the quest
-function goingOnQuest(lst, bool) {
-    var quest = {
-      attended : lst,
-      bool : bool
-    }
-    quests.push(quest)
-    localStorage.setItem("QUESTS", quests)
-    alert(quests.length)
-    setQuestState(quests.length, bool)
-}
 
 // Sets the state to a blue/red background
-function setQuestState(num, bool) {
-    var classList = document.getElementsByClassName("quest-icon");
-    for (var j = 0; j < classList.length; j++) {
-        if (j === num -1) {
-            if (bool === 1) {
-                classList[j].src = "images/result/success.png";
-            } else {
-                classList[j].src = "images/result/fail.png";
-            }
+function setQuestState() {
+    var summaryOfQuest = localStorage.getItem("RESULT").split(",");
+    for (var j = 0; j < summaryOfQuest.length; j++) {
+        if (parseInt(summaryOfQuest[i]) === 1) {
+            classList[j].src = "images/result/success.png";
+        } else {
+            classList[j].src = "images/result/fail.png";
         }
     }
 }
