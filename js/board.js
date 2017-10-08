5805e25105f3c3ba1775924b7b7768f74abfca40
Jahongir A <18970785+jahoffgir@users.noreply.github.com>
2017-10-08 10:17:22 -0400

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1,6 +1,8 @@
 function initBoard() {
-    var counter = 0;
-    localStorage.setItem("COUNTER", counter)
+    localStorage.setItem("COUNTER", 0);
+    var arrys = [];
+    arrys.push("0");
+    localStorage.setItem("IMAGEARR", arrys);
 }
 
 // Sets the state to a blue/red background
@@ -9,17 +11,27 @@ function setQuestState() {
     var summaryOfQuest = localStorage.getItem("RESULT");
 
     var classList = document.getElementsByClassName("quest-icon");
-    console.log(classList);
+
     var a = parseInt(localStorage.getItem("COUNTER"));
-    console.log(a)
-    if (parseInt(summaryOfQuest) === 1) {
-        classList[a].src = "images/result/success.png";
+    alert("counter is " + a);
+    var arr  = localStorage.getItem("IMAGEARR").split(",");
+
+    if (a === 0) {
+        arr[0] = "" + a;
     } else {
-        classList[a].src = "images/result/fail.png";
+        arr.push("" + summaryOfQuest)
+    }
+
+    localStorage.setItem("IMAGEARR", arr);
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === "1") {
+            classList[i].src = "images/result/success.png";
+        } else {
+            classList[i].src = "images/result/fail.png";
+        }
     }
     a++;
     localStorage.setItem("COUNTER", a);
-    alert(calculateNumberOfPeople());
 }
 
 function stateContent(ele) {
@@ -29,6 +41,7 @@ function stateContent(ele) {
 function calculateNumberOfPeople() {
     var arr = localStorage.getItem("RULE").split(",");
     var amountOfPeople = arr[parseInt(localStorage.getItem("COUNTER"))];
-    localStorage.setItem("NUMBER_OF_PLAYERS_PER_QUEST", amountOfPeople);
+    localStorage.setItem("NUMBER_OF_PLAYERS_PER_QUEST", parseInt(amountOfPeople));
+    alert(amountOfPeople + "amount of people");
     // return amountOfPeople;
 }
